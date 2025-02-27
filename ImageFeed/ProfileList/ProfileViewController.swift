3fe8119b33396bc60911ca5badcd822d7113a59d
Denis Kel <asket2013@yandex.ru>
2025-02-27 16:24:57 +1000

    complete vivDidLoad from 9 sprint

---
+++ ImageFeed/ProfileList/ProfileViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Денис Кель on 10.02.2025.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private weak var avatarImage: UIImageView!
+    @IBOutlet private weak var logoutButton: UIButton!
+    @IBOutlet private weak var userNameLabel: UILabel!
+    @IBOutlet private weak var loginNameLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    
+    @IBAction private func didTapLogoutButton(_ sender: Any) {
+        
+    }
+    
+}
+
